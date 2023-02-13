360ef93cfaf26e18005c5dfde2be8a9c59faf9fe
Kristof Sarkozi <krisi.sarkozi@gmail.com>
2023-02-13 14:24:11 +0100

    2D game with no ray casting

--- player.py
+++ player.py
@@ -33,8 +33,7 @@ class Player:
             dx -= speed_sin
             dy += speed_cos
 
-        self.x += dx
-        self.y += dy
+        self.check_wall_collision(dx, dy)
 
         if keys[pg.K_LEFT]:
             self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
@@ -42,6 +41,18 @@ class Player:
             self.angle += PLAYER_ROT_SPEED * self.game.delta_time
 
         self.angle %= math.tau
+
+    def check_wall(self, x, y):
+        return (x, y) not in self.game.map.world_map
+
+    def check_wall_collision(self, dx, dy):
+        if self.check_wall(int(self.x + dx), int(self.y)):
+            self.x += dx
+        if self.check_wall(int(self.x), int(self.y + dy)):
+            self.y += dy
+
+
+
 
     def draw(self):
         pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)

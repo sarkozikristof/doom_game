ccb0316358070e53a7b0c6d1be665a915b5d3c14
Kristof Sarkozi <krisi.sarkozi@gmail.com>
2023-03-11 01:36:57 +0100

    NPC added

--- object_handler.py
+++ object_handler.py
@@ -1,10 +1,13 @@
 from sprite_object import SpriteObject, AnimatedSprite
+from npc import NPC
 
 
 class ObjectHandler:
     def __init__(self, game):
         self.game = game
         self.sprite_list = []
+        self.npc_list = []
+        self.npc_sprite_path = 'resources/sprites/npc/'
         self.static_sprite_path = 'resources/sprites/static_sprites'
         self.animated_sprite_path = 'resources/sprites/animated_sprites'
 
@@ -12,8 +15,14 @@ class ObjectHandler:
         self.add_sprite(AnimatedSprite(game, pos=(1.5, 1.5)))
         self.add_sprite(AnimatedSprite(game, pos=(1.5, 7.5)))
 
+        self.add_npc(NPC(game))
+
     def update(self):
         [sprite.update() for sprite in self.sprite_list]
+        [npc.update() for npc in self.npc_list]
 
     def add_sprite(self, sprite):
         self.sprite_list.append(sprite)
+
+    def add_npc(self, npc):
+        self.npc_list.append(npc)
--- sound.py
+++ sound.py
@@ -5,7 +5,11 @@ class Sound:
     def __init__(self, game):
         self.game = game
         pg.mixer.init()
-        self.path = 'resources/sound'
+        self.path = 'resources/sound/'
         self.shotgun = pg.mixer.Sound(self.path + '/shotgun.mp3')
         self.theme = pg.mixer.Sound(self.path + '/theme.ogg')
         self.theme.set_volume(0.2)
+        self.npc_pain = pg.mixer.Sound(self.path + 'npc_pain.wav')
+        self.npc_death = pg.mixer.Sound(self.path + 'npc_death.wav')
+        self.npc_shot = pg.mixer.Sound(self.path + 'npc_attack.wav')
+        self.player_pain = pg.mixer.Sound(self.path + 'player_pain.wav')

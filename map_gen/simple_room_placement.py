e1347ebcc45bd8d313458c0a5081f47bd57b7052
Kristof Sarkozi <krisi.sarkozi@gmail.com>
2023-08-16 21:42:12 +0200

    Refactoring simple map placement

--- map_gen/simple_room_placement.py
+++ map_gen/simple_room_placement.py
@@ -1,61 +1,75 @@
 import random
 
-WIDTH = 64
-HEIGHT = 64
-ROOM_COUNT = 4
+WIDTH = 10
+HEIGHT = 10
+ROOM_COUNT = 2
 MIN_ROOM_WIDTH = 4
 MAX_ROOM_WIDTH = 10
 MIN_ROOM_HEIGHT = 4
 MAX_ROOM_HEIGHT = 10
 
 
+class Room:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+
+
 class Generate:
     MAP = []
+    ROOMS = []
+
+    def __init__(self):
+        for col in range(WIDTH):
+            _row = []
+            for row in range(HEIGHT):
+                _row.append(False)
+            self.MAP.append(_row)
 
     def generate(self):
-        self._make_map()
-        for _ in range(ROOM_COUNT):
-            room = self._get_a_random_room()
-            self._place_a_room(room)
+        while len(self.ROOMS) != ROOM_COUNT:
+            room = self._get_room()
+            if self._is_correct_position(room):
+                self._place_room(room)
+                self.ROOMS.append(room)
 
         return self.MAP
 
-    def _make_map(self):
-        for row in range(HEIGHT):
-            _row = []
-            for col in range(WIDTH):
-                if col == 0 or col == WIDTH - 1 or row == 0 or row == HEIGHT - 1:
-                    _row.append(1)
-                else:
-                    _row.append(False)
-            self.MAP.append(_row)
+    @staticmethod
+    def _get_room():
+        r_w = random.randint(MIN_ROOM_WIDTH, MAX_ROOM_WIDTH)
+        r_h = random.randint(MIN_ROOM_HEIGHT, MAX_ROOM_HEIGHT)
+        r_x = random.randint(0, WIDTH - r_w)
+        r_y = random.randint(0, HEIGHT - r_h)
 
-    def _get_a_random_room(self):
-        room_start_x_pos = random.randint(0, WIDTH - MIN_ROOM_WIDTH)
+        return Room(r_x, r_y, r_w, r_h)
 
-        room_start_y_pos = random.randint(0, HEIGHT - MIN_ROOM_HEIGHT)
-
-        room_width = random.randint(MIN_ROOM_WIDTH, MAX_ROOM_WIDTH)
-        room_height = random.randint(MAX_ROOM_HEIGHT, MAX_ROOM_WIDTH)
-
-        room = room_start_x_pos, room_start_y_pos, room_width, room_height
-
-        if self._check_room_placement(room):
-            return room
-        else:
-            self._get_a_random_room()
+    def _is_correct_position(self, room):
+        if self._is_out_of_map_boundaries(room) and self._is_overlapping_other_room(room):
+            return True
+        return False
 
     @staticmethod
-    def _check_room_placement(room):
-        # checking if in boundaries
-        if room[0] + room[2] > WIDTH and room[1] + room[3] > HEIGHT:
+    def _is_out_of_map_boundaries(room):
+        if room.x + room.width > WIDTH:
+            return False
+        if room.y + room.height > HEIGHT:
             return False
         return True
 
-    def _place_a_room(self, room):
-        for i in range(room[2]):
-            for j in range(room[3]):
-                self.MAP[room[0] + i][room[1] + j] = 1
+    def _is_overlapping_other_room(self, room):
+        for width_step in range(room.width):
+            for height_step in range(room.height):
+                if self.MAP[room.x + width_step][room.y + height_step] is True:
+                    return False
+        return True
+
+    def _place_room(self, room):
+        for width_step in range(room.width):
+            for height_step in range(room.height):
+                self.MAP[room.x + width_step][room.y + height_step] = 1
 
 
 mini_map = Generate().generate()
